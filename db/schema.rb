b93d909cbc6e6062f45605b058e2ec41e7cee2d0
AppleDash <derp@derpy.fr>
2016-09-05 23:23:33 -0400

    Drop in Postgres and begin work on main page

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160905225415) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string   "url"
     t.datetime "time"
